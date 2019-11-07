ffe1750749a0c30b4201b50de1d96f66fa898bb1
Raposo <william.s.godoy53@gmail.com>
2019-11-07 02:44:40 -0300

    Base App style

---
+++ src/App.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 800px;
+  width: 100%;
+  margin: 100px auto 100px;
+  background-color: #fdfdfd;
+  border-radius: 7px;
+  padding: 15px;
+  box-shadow: 0 0 6px -2px #bebebe;
+  display: flex;
+  height: 100%;
+`
+
+export {
+  Container
+}

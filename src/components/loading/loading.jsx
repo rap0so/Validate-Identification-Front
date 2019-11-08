f0d14f00e486f648861925d3ef2a5cdc4071e2d5
Raposo <william.s.godoy53@gmail.com>
2019-11-08 02:07:27 -0300

    Improving Loading componenht

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import loadingIcon from './Rolling-1s-200px.svg'
 
-const Loading = () => <img src={loadingIcon} />
+const loadingStyle = { maxWidth: '130px', margin: 'auto' }
+
+const Loading = () => <img style={loadingStyle} src={loadingIcon} />
 
 export default Loading

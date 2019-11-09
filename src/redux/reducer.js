341b513e309924972fb98f6ee203a68928f5d2d0
Raposo <william.s.godoy53@gmail.com>
2019-11-09 17:05:31 -0300

    Reversing result on reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -23,7 +23,7 @@ const store = (state = initialState, action) => {
           id.parsedNumber = id.number.replace(/^([\d]{2})([\d]{3})([\d]{3})([\d]{4})([\d]{2})$/, '$1.$2.$3/$4-$5')
         }
         return id
-      })
+      }).reverse()
       return ({
         loading: false,
         data: action.data

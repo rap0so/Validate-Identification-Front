2e693f4e4b3f4110f39114838031dacc2c301a87
Raposo <william.s.godoy53@gmail.com>
2019-11-09 00:46:38 -0300

    Parsing result to show something better to final user

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -15,6 +15,15 @@ const store = (state = initialState, action) => {
         loading: true
       }
     case RECEIVE_IDENTIFIERS:
+      action.data = action.data.map(id => {
+        const currentNumber = id.number
+        if (currentNumber.length === 11) {
+          id.parsedNumber = id.number.replace(/^([\d]{3})([\d]{3})([\d]{3})([\d]{2})$/, '$1.$2.$3-$4')
+        } else {
+          id.parsedNumber = id.number.replace(/^([\d]{2})([\d]{3})([\d]{3})([\d]{4})([\d]{2})$/, '$1.$2.$3/$4-$5')
+        }
+        return id
+      })
       return ({
         loading: false,
         data: action.data
